f2e58273f394779b151af70336313b8337222866
debajyoti-ghosh <debajyoti.ghosh.87@gmail.com>
2021-10-03 00:17:38 +0530

    Escape Sequences

---
+++ Exe10.py
@@ -0,0 +1,19 @@
+hight_1 = "I am 6'2\" tall"
+hight_2 = 'I\'m 6\'2" tall'
+
+tabby_cat = "\t I'm tabbed in."
+persian_cat = "I'm split\non a line."
+bachslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+print(hight_1)
+print(hight_2)
+print(tabby_cat)
+print(persian_cat)
+print(bachslash_cat)
+print(fat_cat)
