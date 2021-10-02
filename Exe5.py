aac2b76d6c22f391520db2b9cdb85514a2e578c5
debajyoti-ghosh <debajyoti.ghosh.87@gmail.com>
2021-10-02 21:15:05 +0530

    More Variables and Printing

---
+++ Exe5.py
@@ -0,0 +1,16 @@
+my_name = 'Debajyoti Ghosh'
+my_age = 33 # not a lie
+my_height = 74 # inches
+my_weight = 86 # kgs
+my_eyes  = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}")
+print(f"He's {my_height} inches tall")
+print(f"He's {my_weight} kgs heavy")
+print("Actually that's not too heavy")
+print(f"He's got {my_eyes} eyes and {my_hair} hair. ")
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight}, I get {total}. ")
