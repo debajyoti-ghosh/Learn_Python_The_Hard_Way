1274d54af4016e67235fcc5d43317ab683164e15
debajyoti-ghosh <debajyoti.ghosh.87@gmail.com>
2021-10-07 12:10:20 +0530

    Function to ask for user input

---
+++ Exe19_user.py
@@ -0,0 +1,8 @@
+def add_number():
+    print("Python is here to add two numbers.")
+    first_number= int(input("Enter the 1st number: "))
+    second_number = int(input("Enter the 2nd number: "))
+    result = first_number +second_number
+    print(f"The result is : {result} ")
+
+add_number()
